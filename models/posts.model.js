f052ba92b0e9a407a536cc1658fcfe75ff94aaa1
unknown <gracefluttech@gmail.com>
2024-12-18 02:38:43 +0530

    posts slug removed

--- models/posts.model.js
+++ models/posts.model.js
@@ -8,7 +8,7 @@ const postSchema = new mongoose.Schema({
     },
     slug: {
         type: String,
-        required: true,
+        required: false,
         unique: true,
         lowercase: true
     },
